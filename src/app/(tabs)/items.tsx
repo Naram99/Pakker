6755c69353840d5df088d03c855f18140e28a888
Naram99 <farkassebi@gmail.com>
2026-03-04 22:06:39 +0100

    first working state

---
+++ src/app/(tabs)/items.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ItemsPage = () => {
+    return (
+        <View>
+            <Text>Items</Text>
+        </View>
+    );
+};
+
+export default ItemsPage;
+
+const styles = StyleSheet.create({});
