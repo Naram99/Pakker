d8632927a8ea5b9529d8ad177b3da2cda7e8b48a
farkas.sebestyen <farkas.sebestyen@ssm.hu>
2026-03-26 16:20:06 +0100

    itemstore addItem function, extracting needed: db, services

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,16 @@
 import { Stack } from "expo-router";
+import { useItemStore } from "../zustand/ItemStore";
+import { useEffect } from "react";
 
 export default function RootLayout() {
     // Future initialization on app start
+
+    // Zustand DB init
+    const itemStoreInit = useItemStore((state) => state.init);
+
+    useEffect(() => {
+        itemStoreInit();
+    }, []);
+
     return <Stack screenOptions={{ headerShown: false }} />;
 }
--- src/zustand/ItemStore.ts
+++ src/zustand/ItemStore.ts
@@ -13,6 +13,7 @@ interface DefaultItem {
     recommendedSex?: string;
     version: number;
     isCustom: false;
+    status: "ready";
 }
 
 interface CustomItem {
@@ -23,6 +24,7 @@ interface CustomItem {
     recommendedSex?: string;
     version: number;
     isCustom: true;
+    status: "ready" | "saving";
 }
 
 type Item = DefaultItem | CustomItem;
@@ -35,9 +37,24 @@ interface ItemStore {
 
     init: () => Promise<void>;
     loadItems: () => Promise<void>;
-    addItem: (item: Item) => Promise<void>;
+
+    addItem: (item: CustomItem) => Promise<void>;
+    addItemToDB: (item: CustomItem) => Promise<number>;
+
     updateItem: (id: number, updates: Partial<Item>) => Promise<void>;
+    updateItemInDB: (id: number, updates: Partial<Item>) => Promise<void>;
+
     deleteItem: (id: number, isCustom: boolean) => Promise<void>;
+    deleteItemFromDB: (id: number, isCustom: boolean) => Promise<void>;
+}
+
+class ItemStoreError extends Error {
+    constructor(
+        public message: string = "",
+        public options: ErrorOptions = {},
+    ) {
+        super(message, options);
+    }
 }
 
 export const useItemStore = create<ItemStore>((set, get) => ({
@@ -47,20 +64,26 @@ export const useItemStore = create<ItemStore>((set, get) => ({
     isLoading: false,
 
     init: async () => {
+        if (get().isReady || get().isLoading) return;
+
+        set({ isLoading: true });
+
         try {
             const database = await initializeDatabase();
             set({ db: database, isReady: true });
-
-            get().loadItems();
         } catch (error) {
             console.error(`ItemStore init failed: ${error}`);
+        } finally {
+            set({ isLoading: false });
         }
     },
 
     loadItems: async () => {
-        set({ isLoading: true });
         const { db } = get();
-        if (!db) return;
+
+        if (!get().isReady || get().isLoading || !db) return;
+
+        set({ isLoading: true });
 
         try {
             const result = await db.getAllAsync<DefaultItem>(
@@ -73,7 +96,92 @@ export const useItemStore = create<ItemStore>((set, get) => ({
             set({ isLoading: false });
         }
     },
-    addItem: async (item: Item) => {},
+
+    addItem: async (item: CustomItem) => {
+        const tmpId = -Date.now();
+
+        const tmpItem: CustomItem = {
+            ...item,
+            id: tmpId,
+            status: "saving",
+        };
+
+        set((state) => ({
+            items: [tmpItem, ...state.items],
+        }));
+
+        try {
+            const insertId = await get().addItemToDB(item);
+
+            set((state) => ({
+                items: state.items.map((i) =>
+                    i.id === tmpId
+                        ? { ...i, id: insertId, status: "ready" }
+                        : i,
+                ),
+            }));
+        } catch (error) {
+            console.error(error);
+            // Rollback
+            set((state) => ({
+                items: state.items.filter((i) => i.id !== tmpId),
+            }));
+        }
+    },
+
+    addItemToDB: async (item: CustomItem) => {
+        const { db } = get();
+
+        if (!get().isReady || !db)
+            throw new ItemStoreError("Database is not available!");
+
+        const query = await db.prepareAsync(`
+            INSERT INTO custom_items (name, is_always_needed, priority, version ${item.recommendedSex ? ", recommended_sex" : ""})
+            VALUES ($name, $isAlwaysNeeded, $priority, $version ${item.recommendedSex ? ", $recommendedSex" : ""})
+        `);
+
+        const insert = await query.executeAsync({
+            $name: item.name,
+            $isAlwaysNeeded: item.isAlwaysNeeded,
+            $priority: item.priority,
+            $version: item.version,
+            $recommendedSex: item.recommendedSex || null,
+        });
+
+        return insert.lastInsertRowId;
+    },
+
     updateItem: async (id: number, updates: Partial<Item>) => {},
-    deleteItem: async (id: number, isCustom: boolean) => {},
+
+    updateItemInDB: async (id: number, updates: Partial<Item>) => {},
+
+    deleteItem: async (id: number, isCustom: boolean) => {
+        const prevItems = get().items;
+
+        set((state) => ({
+            items: state.items.filter((item) => item.id !== id),
+        }));
+
+        try {
+            await get().deleteItemFromDB(id, isCustom);
+        } catch (error) {
+            console.error(error);
+            // Rollback
+            set({ items: prevItems });
+        }
+    },
+
+    deleteItemFromDB: async (id: number, isCustom: boolean) => {
+        const { db } = get();
+
+        if (!get().isReady || !db)
+            throw new ItemStoreError("Database is not available!");
+
+        // Foreign keys !?
+        const query = await db.prepareAsync(
+            `DELETE FROM ${isCustom ? "custom_items" : "default_items"} WHERE id = $id`,
+        );
+
+        await query.executeAsync({ $id: id });
+    },
 }));
